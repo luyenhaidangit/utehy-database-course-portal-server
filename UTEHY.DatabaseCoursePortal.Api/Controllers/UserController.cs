55010827a216f2c23ad6c189b8d32172f4758d35
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2023-09-07 00:25:34 +0700

    Upgrade api login with validation and separate env jwt

--- UTEHY.DatabaseCoursePortal.Api/Controllers/UserController.cs
+++ UTEHY.DatabaseCoursePortal.Api/Controllers/UserController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
@@ -16,53 +15,66 @@ namespace UTEHY.DatabaseCoursePortal.Api.Controllers
     public class UserController : ControllerBase
     {
         private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly IConfiguration _config;
 
-        public UserController(UserManager<User> userManager)
+        public UserController(UserManager<User> userManager, SignInManager<User> signInManager, IConfiguration config)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
+            _config = config;
         }
 
         [HttpPost]
         [Route("login")]
         public async Task<ApiResult<string>> Login([FromBody] LoginRequest request)
         {
+            //Verify
             var user = await _userManager.FindByNameAsync(request.UserName);
 
-            if (user != null && await _userManager.CheckPasswordAsync(user, request.Password))
-            {
-                var claims = new List<Claim>
-                {
-                    new Claim(ClaimTypes.Name, user.UserName),
-                    new Claim(ClaimTypes.Email, user.Email),
-                };
-
-                var claimsIdentity = new ClaimsIdentity(claims, "JwtBearer", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
-
-                var tokenDescriptor = new SecurityTokenDescriptor
-                {
-                    Subject = claimsIdentity,
-                    Expires = DateTime.UtcNow.AddDays(7),
-                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes("lRiOjKQlNLfPZAzW8ZIiR1Ls2QbG9dUc9mpb3T6TqTm")), SecurityAlgorithms.HmacSha256Signature)
-                };
-
-                var tokenHandler = new JwtSecurityTokenHandler();
-                var token = tokenHandler.CreateToken(tokenDescriptor);
-
-                return new ApiResult<string>()
-                {
-                    Status = true,
-                    Message = "Đăng nhập thành công!",
-                    Data = tokenHandler.WriteToken(token)
-                };
-            }
-            else
+            if (user == null)
             {
                 return new ApiResult<string>()
                 {
                     Status = false,
-                    Message = "Đăng nhập thất bại!"
+                    Message = "Tên người dùng không tồn tại!",
                 };
             }
+
+            var result = await _signInManager.PasswordSignInAsync(user, request.Password, request.RememberMe, true);
+
+            if(!result.Succeeded) 
+            {
+                return new ApiResult<string>()
+                {
+                    Status = false,
+                    Message = "Tài khoản hoặc mật khẩu người dùng không hợp lệ!",
+                };
+            }
+
+            //Create token
+            var issuer = _config["Jwt:Issuer"];
+            var audience = _config["Jwt:Audience"];
+            var roles = await _userManager.GetRolesAsync(user);
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Role, string.Join(";",roles)),
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var expires = DateTime.UtcNow.AddDays(30);
+
+            var token = new JwtSecurityToken(issuer,audience,claims,expires,signingCredentials: creds);
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            return new ApiResult<string>()
+            {
+                Status = true,
+                Message = "Đăng nhập thành công!",
+                Data = tokenHandler.WriteToken(token)
+            };
         }
     }
 }
--- UTEHY.DatabaseCoursePortal.Api/Models/User/LoginRequest.cs
+++ UTEHY.DatabaseCoursePortal.Api/Models/User/LoginRequest.cs
@@ -1,9 +1,19 @@
-﻿namespace UTEHY.DatabaseCoursePortal.Api.Models.UserViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UTEHY.DatabaseCoursePortal.Api.Models.UserViewModels
 {
     public class LoginRequest
     {
+        [Required]
+        [MinLength(4)]
+        [MaxLength(40)]
         public string? UserName { get; set; }
 
+        [Required]
+        [MinLength(4)]
+        [MaxLength(40)]
         public string? Password { get; set; }
+
+        public bool RememberMe { get; set; }
     }
 }
